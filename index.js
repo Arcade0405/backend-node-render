1458d21a1807c622d16c325c6d7fc7417ef4647d
Arcade0405 <minhnhut2004@gmail.com>
2025-12-19 11:56:45 +0700

    test1

--- index.js
+++ index.js
@@ -5,9 +5,15 @@ mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log("MongoDB connected"))
     .catch(err => console.error("❌ MongoDB connection error:", err));
 const app = express();
+app.use(cors()); // Cho phép frontend gọi API từ domain khác (Netlify)
 
 app.get("/", (req, res) => {
-    res.json({ message: "Backend CI/CD OK"});
+  res.send("Backend Node.js đang hoạt động 🚀");
 });
 
-app.listen(3000);
+app.get("/api/hello", (req, res) => {
+  res.json({ message: "Xin chào từ backend Node.js 👋" });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server đang chạy tại cổng ${PORT}`));
